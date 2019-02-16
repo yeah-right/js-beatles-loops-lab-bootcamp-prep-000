523aea7450422163d354e3b6d6138afeee684f8b
Zack A Claar <claar.zack@gmail.com>
2019-02-16 19:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ function theBeatlesPlay(musicians, instruments) {
   return tempArray;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 function johnLennonFacts(facts) {
-  
+  while 
 }
