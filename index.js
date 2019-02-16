ad25c73c76c2a24062fa1b06e7869e3e660d2100
Zack A Claar <claar.zack@gmail.com>
2019-02-16 19:10:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // add solution here
-function theBeatlesPlay()
+function theBeatlesPlay(musicians, instruments) {
+  var tempArray = [];
+  
+  for (i = 0; i < musicians.length; i++) {
+    
+  }
+}
