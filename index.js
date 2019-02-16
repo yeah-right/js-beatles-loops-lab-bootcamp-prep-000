6f4505996420d12ac48f7fe41729246b84441891
Zack A Claar <claar.zack@gmail.com>
2019-02-16 19:13:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var tempArray = [];
   
-  for (i = 0; i < musicians.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     tempArray.push(musicians[i] + " plays " + instruments[i]);
   }
 }
